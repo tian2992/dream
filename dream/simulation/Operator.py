806a7f3d5eaf81e562a8a89fb7a6609722941d7f
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2014-08-21 14:58:56 +0100

    Object Resources except OperatorPool to read in new style

--- dream/simulation/Operator.py
+++ dream/simulation/Operator.py
@@ -42,7 +42,7 @@ class Operator(ObjectResource):
         ObjectResource.__init__(self)
         self.id=id
         self.objName=name
-        self.capacity=capacity      # repairman is an instance of resource
+        self.capacity=int(capacity)      # repairman is an instance of resource
         self.type="Operator"
         # lists to hold statistics of multiple runs
         self.Waiting=[]             # holds the percentage of waiting time 
@@ -79,6 +79,8 @@ class Operator(ObjectResource):
         self.skillsList=skills
         # flag to show if the resource is available at the start of simulation
         self.available=available
+        from Globals import G
+        G.OperatorsList.append(self) 
         
     @staticmethod
     def getSupportedSchedulingRules():
