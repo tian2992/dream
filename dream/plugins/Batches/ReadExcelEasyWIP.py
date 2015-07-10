07798c99ba85ed79d97d8151e57bdbd546de7911
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-07-10 09:44:11 +0100

    plugin to create buffered batches

--- dream/plugins/Batches/ReadExcelEasyWIP.py
+++ dream/plugins/Batches/ReadExcelEasyWIP.py
@@ -15,7 +15,75 @@ class ReadExcelEasyWIP(plugin.InputPreparationPlugin):
     def preprocess(self, data):
         nodes=data['graph']['node']
         data_uri_encoded_input_data = data['input'].get(self.configuration_dict['input_id'], {})      
-        print data_uri_encoded_input_data
+        WIPData=self.convertExcelToList(data_uri_encoded_input_data, "WIP")
+        from pprint import pprint
+        # pprint(WIPData)
+        
+        # get the number of units for a standard batch
+        standardBatchUnits=0
+        for node_id, node in nodes.iteritems():
+            if node['_class']=='Dream.BatchSource':
+                standardBatchUnits=int(node['batchNumberOfUnits']) 
+            node['wip']=[]
+        
+        WIPData.pop(0)
+        
+        for row in WIPData:
+            stationId=row[0]
+            if not stationId:
+                continue        
+            workingBatchSize=nodes[stationId]['workingBatchSize']    
+            assert stationId in nodes.keys(), "WIP spreadsheet has "+ stationId +" that does not exist in the graph"  
+            buffered=row[1]
+            # check for buffered batches of sub-batches
+            if buffered:
+                # get the buffer id and also if there is decomposition after the buffer
+                current=stationId
+                decomposition=False
+                while 1: 
+                    previous=self.getPredecessors(data, current)[0]
+                    if nodes[previous]['_class'] in ['Dream.Queue','Dream.LineClearance','Dream.RoutingQueue']:
+                        bufferId=previous
+                        break
+                    if 'Decomposition' in nodes[previous]['_class']:
+                        decomposition=True
+                    current=previous
+                # check if we have batches or sub-batches
+                if decomposition or workingBatchSize==standardBatchUnits:
+                    _class='Dream.Batch'
+                else:
+                    _class='Dream.SubBatch'
+                # if we have batches
+                if _class=='Dream.Batch':
+                    numberOfBatches=int(buffered/standardBatchUnits)
+                    for i in range(numberOfBatches):
+                        partId=stationId+'_'+str(i)
+                        wipDict={
+                          "_class": _class,
+                          "id": partId, 
+                          "name": 'Batch_'+partId+'_wip',
+                          "numberOfUnits":standardBatchUnits,
+                          }
+                        nodes[bufferId]['wip'].append(wipDict)
+                elif _class=='Dream.SubBatch':
+                    numberOfBatches=int(buffered/standardBatchUnits)
+                    numberOfSubBatches=int(standardBatchUnits/workingBatchSize)
+                    for i in range(numberOfBatches):
+                        parentBatchId=stationId+'_'+str(i)
+                        for j in range(numberOfSubBatches):
+                            partId=stationId+'_'+parentBatchId+'_'+str(j)
+                            wipDict={
+                              "_class": _class,
+                              "id": partId, 
+                              "name": 'Batch_'+partId+'_wip',
+                              "numberOfUnits":workingBatchSize,
+                              "parentBatchId":parentBatchId,
+                              "name":'Batch'+parentBatchId+'_SB'+partId+'wip'
+                              } 
+                            nodes[bufferId]['wip'].append(wipDict)
+            for node_id, node in nodes.iteritems():
+                pass
+                #print node_id,len(node['wip'])
         return data
 
     
